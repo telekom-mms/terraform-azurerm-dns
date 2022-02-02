f0f52e9cb32887d0e6bbcf1924688598ea26cfd1
mima <Michaela.Mattes@t-systems.com>
2022-02-02 16:33:39 +0100

    fmt

--- examples/main.tf
+++ examples/main.tf
@@ -1,12 +1,12 @@
 module "dns" {
-  source           = "../terraform-dns"
-  dns_zone            = {
-    domain_com  = {
-      name = "domain.com"
+  source = "../terraform-dns"
+  dns_zone = {
+    domain_com = {
+      name                = "domain.com"
       resource_group_name = "service-infrastructure-rg"
     }
-    domain_de  = {
-      name = "domain.de"
+    domain_de = {
+      name                = "domain.de"
       resource_group_name = "service-infrastructure-rg"
     }
   }
