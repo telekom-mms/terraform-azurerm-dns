049f74d4d1dee525de2524bf1b6ecdf50def49c6
mima <Michaela.Mattes@t-systems.com>
2022-03-16 13:03:42 +0100

    add more output arguments

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,14 @@ output "dns_zone" {
     }
   }
 }
+output "dns_a_record" {
+  description = "azurerm_dns_a_record results"
+  value = {
+    for dns_a_record in keys(azurerm_dns_a_record.dns_a_record) :
+    dns_a_record => {
+      id   = azurerm_dns_a_record.dns_a_record[dns_a_record].id
+      name = azurerm_dns_a_record.dns_a_record[dns_a_record].name
+      fqdn = azurerm_dns_a_record.dns_a_record[dns_a_record].fqdn
+    }
+  }
+}
