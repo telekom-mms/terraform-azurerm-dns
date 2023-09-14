720ed1a5e8feba9895072c3c2271319e021894ea
Michaela Mattes <m.mattes@telekom.de>
2023-09-14 13:54:13 +0200

    refactor module

--- main.tf
+++ main.tf
@@ -1,31 +1,61 @@
 /**
- * # dns
- *
- * This module manages Azure DNS Zones.
- *
+* # dns
+*
+* This module manages the hashicorp/azurerm dns resources.
+* For more information see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs > dns
+*
 */
 
-/** DNS Zone */
 resource "azurerm_dns_zone" "dns_zone" {
   for_each = var.dns_zone
 
   name                = local.dns_zone[each.key].name == "" ? each.key : local.dns_zone[each.key].name
   resource_group_name = local.dns_zone[each.key].resource_group_name
 
+  dynamic "soa_record" {
+    for_each = local.dns_zone[each.key].soa_record == null ? [] : [0]
+
+    content {
+      email         = local.dns_zone[each.key].soa_record.email
+      host_name     = local.dns_zone[each.key].soa_record.host_name
+      expire_time   = local.dns_zone[each.key].soa_record.expire_time
+      minimum_ttl   = local.dns_zone[each.key].soa_record.minimum_ttl
+      refresh_time  = local.dns_zone[each.key].soa_record.refresh_time
+      retry_time    = local.dns_zone[each.key].soa_record.retry_time
+      serial_number = local.dns_zone[each.key].soa_record.serial_number
+      ttl           = local.dns_zone[each.key].soa_record.ttl
+      tags          = local.dns_zone[each.key].soa_record.tags
+    }
+  }
+
   tags = local.dns_zone[each.key].tags
 }
 
-/** Private DNS Zone */
 resource "azurerm_private_dns_zone" "private_dns_zone" {
   for_each = var.private_dns_zone
 
   name                = local.private_dns_zone[each.key].name == "" ? each.key : local.private_dns_zone[each.key].name
   resource_group_name = local.private_dns_zone[each.key].resource_group_name
 
+  dynamic "soa_record" {
+    for_each = local.private_dns_zone[each.key].soa_record == null ? [] : [0]
+
+    content {
+      email         = local.private_dns_zone[each.key].soa_record.email
+      host_name     = local.private_dns_zone[each.key].soa_record.host_name
+      expire_time   = local.private_dns_zone[each.key].soa_record.expire_time
+      minimum_ttl   = local.private_dns_zone[each.key].soa_record.minimum_ttl
+      refresh_time  = local.private_dns_zone[each.key].soa_record.refresh_time
+      retry_time    = local.private_dns_zone[each.key].soa_record.retry_time
+      serial_number = local.private_dns_zone[each.key].soa_record.serial_number
+      ttl           = local.private_dns_zone[each.key].soa_record.ttl
+      tags          = local.private_dns_zone[each.key].soa_record.tags
+    }
+  }
+
   tags = local.private_dns_zone[each.key].tags
 }
 
-/** A Records */
 resource "azurerm_dns_a_record" "dns_a_record" {
   for_each = var.dns_a_record
 
@@ -39,7 +69,6 @@ resource "azurerm_dns_a_record" "dns_a_record" {
   tags = local.dns_a_record[each.key].tags
 }
 
-/** CNAME Records */
 resource "azurerm_dns_cname_record" "dns_cname_record" {
   for_each = var.dns_cname_record
 
@@ -48,12 +77,11 @@ resource "azurerm_dns_cname_record" "dns_cname_record" {
   zone_name           = local.dns_cname_record[each.key].zone_name
   ttl                 = local.dns_cname_record[each.key].ttl
   record              = local.dns_cname_record[each.key].record
+  target_resource_id  = local.dns_cname_record[each.key].target_resource_id
 
   tags = local.dns_cname_record[each.key].tags
 }
 
-
-/** TXT Records */
 resource "azurerm_dns_txt_record" "dns_txt_record" {
   for_each = var.dns_txt_record
 
@@ -64,6 +92,7 @@ resource "azurerm_dns_txt_record" "dns_txt_record" {
 
   dynamic "record" {
     for_each = local.dns_txt_record[each.key].record
+
     content {
       value = local.dns_txt_record[each.key].record[record.key].value
     }
@@ -72,7 +101,6 @@ resource "azurerm_dns_txt_record" "dns_txt_record" {
   tags = local.dns_txt_record[each.key].tags
 }
 
-/** MX Records */
 resource "azurerm_dns_mx_record" "dns_mx_record" {
   for_each = var.dns_mx_record
 
@@ -81,9 +109,13 @@ resource "azurerm_dns_mx_record" "dns_mx_record" {
   zone_name           = local.dns_mx_record[each.key].zone_name
   ttl                 = local.dns_mx_record[each.key].ttl
 
-  record {
-    preference = local.dns_mx_record[each.key].record.preference
-    exchange   = local.dns_mx_record[each.key].record.exchange
+  dynamic "record" {
+    for_each = local.dns_mx_record[each.key].record
+
+    content {
+      preference = local.dns_mx_record[each.key].record[record.key].preference
+      exchange   = local.dns_mx_record[each.key].record[record.key].exchange
+    }
   }
 
   tags = local.dns_mx_record[each.key].tags
--- outputs.tf
+++ outputs.tf
@@ -1,67 +1,101 @@
 output "dns_zone" {
-  description = "azurerm_dns_zone results"
+  description = "Outputs all attributes of resource_type."
   value = {
     for dns_zone in keys(azurerm_dns_zone.dns_zone) :
     dns_zone => {
-      id                  = azurerm_dns_zone.dns_zone[dns_zone].id
-      name                = azurerm_dns_zone.dns_zone[dns_zone].name
-      resource_group_name = azurerm_dns_zone.dns_zone[dns_zone].resource_group_name
+      for key, value in azurerm_dns_zone.dns_zone[dns_zone] :
+      key => value
     }
   }
 }
+
 output "private_dns_zone" {
-  description = "azurerm_private_dns_zone results"
+  description = "Outputs all attributes of resource_type."
   value = {
     for private_dns_zone in keys(azurerm_private_dns_zone.private_dns_zone) :
     private_dns_zone => {
-      id                        = azurerm_private_dns_zone.private_dns_zone[private_dns_zone].id
-      number_of_record_sets     = azurerm_private_dns_zone.private_dns_zone[private_dns_zone].number_of_record_sets
-      max_number_of_record_sets = azurerm_private_dns_zone.private_dns_zone[private_dns_zone].max_number_of_record_sets
-      soa_record                = azurerm_private_dns_zone.private_dns_zone[private_dns_zone].soa_record
+      for key, value in azurerm_private_dns_zone.private_dns_zone[private_dns_zone] :
+      key => value
     }
   }
 }
+
 output "dns_a_record" {
-  description = "azurerm_dns_a_record results"
+  description = "Outputs all attributes of resource_type."
   value = {
     for dns_a_record in keys(azurerm_dns_a_record.dns_a_record) :
     dns_a_record => {
-      id   = azurerm_dns_a_record.dns_a_record[dns_a_record].id
-      name = azurerm_dns_a_record.dns_a_record[dns_a_record].name
-      fqdn = azurerm_dns_a_record.dns_a_record[dns_a_record].fqdn
+      for key, value in azurerm_dns_a_record.dns_a_record[dns_a_record] :
+      key => value
     }
   }
 }
+
 output "dns_cname_record" {
-  description = "azurerm_dns_cname_record results"
+  description = "Outputs all attributes of resource_type."
   value = {
     for dns_cname_record in keys(azurerm_dns_cname_record.dns_cname_record) :
     dns_cname_record => {
-      id   = azurerm_dns_cname_record.dns_cname_record[dns_cname_record].id
-      name = azurerm_dns_cname_record.dns_cname_record[dns_cname_record].name
-      fqdn = azurerm_dns_cname_record.dns_cname_record[dns_cname_record].fqdn
+      for key, value in azurerm_dns_cname_record.dns_cname_record[dns_cname_record] :
+      key => value
     }
   }
 }
+
 output "dns_txt_record" {
-  description = "azurerm_dns_txt_record results"
+  description = "Outputs all attributes of resource_type."
   value = {
     for dns_txt_record in keys(azurerm_dns_txt_record.dns_txt_record) :
     dns_txt_record => {
-      id   = azurerm_dns_txt_record.dns_txt_record[dns_txt_record].id
-      name = azurerm_dns_txt_record.dns_txt_record[dns_txt_record].name
-      fqdn = azurerm_dns_txt_record.dns_txt_record[dns_txt_record].fqdn
+      for key, value in azurerm_dns_txt_record.dns_txt_record[dns_txt_record] :
+      key => value
     }
   }
 }
+
 output "dns_mx_record" {
-  description = "azurerm_dns_mx_record results"
+  description = "Outputs all attributes of resource_type."
   value = {
     for dns_mx_record in keys(azurerm_dns_mx_record.dns_mx_record) :
     dns_mx_record => {
-      id   = azurerm_dns_mx_record.dns_mx_record[dns_mx_record].id
-      name = azurerm_dns_mx_record.dns_mx_record[dns_mx_record].name
-      fqdn = azurerm_dns_mx_record.dns_mx_record[dns_mx_record].fqdn
+      for key, value in azurerm_dns_mx_record.dns_mx_record[dns_mx_record] :
+      key => value
+    }
+  }
+}
+
+output "variables" {
+  description = "Displays all configurable variables passed by the module. __default__ = predefined values per module. __merged__ = result of merging the default values and custom values passed to the module"
+  value = {
+    default = {
+      for variable in keys(local.default) :
+      variable => local.default[variable]
+    }
+    merged = {
+      dns_zone = {
+        for key in keys(var.dns_zone) :
+        key => local.dns_zone[key]
+      }
+      private_dns_zone = {
+        for key in keys(var.private_dns_zone) :
+        key => local.private_dns_zone[key]
+      }
+      dns_a_record = {
+        for key in keys(var.dns_a_record) :
+        key => local.dns_a_record[key]
+      }
+      dns_cname_record = {
+        for key in keys(var.dns_cname_record) :
+        key => local.dns_cname_record[key]
+      }
+      dns_txt_record = {
+        for key in keys(var.dns_txt_record) :
+        key => local.dns_txt_record[key]
+      }
+      dns_mx_record = {
+        for key in keys(var.dns_mx_record) :
+        key => local.dns_mx_record[key]
+      }
     }
   }
 }
